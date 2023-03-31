274f9908315c1199b7af015a1bc5489c7919e8aa
github_username <you@domain.com>
2023-03-31 06:54:41 +0600

    load data

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './componet/Header/Header';
+import Home from './componet/Home/Home';
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Home></Home>
     
     </div>
   )
--- src/componet/Home/Home.jsx
+++ src/componet/Home/Home.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css';
+
+const loadDataDisplay =()=>{
+    // const [data, setData] = useState([]);
+    useEffect(()=>{
+
+        fetch('/public/fakeData.json')
+        .then(res => res.json())
+        .then(data => console.log(data))
+    },[])
+
+}
 
 const Home = () => {
     return (
         <div>
-            <h4>Home</h4>
+           <div className="container mt-5 row row-cols-1">
+            <div className="card-right  col-md-9">
+             
+            </div>
+            <div className="card-left card col-md-3">
+                <h4>aside</h4>
+
+            </div>
+           </div>
         </div>
     );
 };
